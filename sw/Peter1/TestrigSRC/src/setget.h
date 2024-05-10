410c22cf03f88e5baeb17e080fabef7aa7960a39
quarc2000 <quarc2000@gmail.com>
2024-05-10 13:15:30 +0200

    Testocode that includes safe read/write

---
+++ sw/Peter1/TestrigSRC/src/setget.h
@@ -0,0 +1,35 @@
+#ifndef GLOBAL_VAR_H
+#define GLOBAL_VAR_H
+
+
+// Define the number of variables
+//#define NUM_VARS 4
+
+
+
+// Create a lookup table for the variable names
+typedef enum {
+    rawDistLeft, 
+    rawDistFront, 
+    rawDistRight, 
+    rawDistBack,
+    rawMagX,
+    rawMagY,
+    rawMagZ,
+    rawAccX,
+    rawAccY,
+    rawAccZ,
+    rawTemp,
+    rawGyX,
+    rawGyY,
+    rawGyZ,
+    NUM_VARS
+} VarNames;
+
+// Function prototypes
+void globalVar_init(void);
+void globalVar_set(VarNames varName, int value);
+int globalVar_get(VarNames varName);
+int globalVar_get(VarNames varName, long *age);
+
+#endif // GLOBAL_VAR_H
