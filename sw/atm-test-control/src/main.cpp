da164eb00da8a4190711da7967e85b48b42b84e3
Andersson Reyna Oskar <oskar.x.andersson@scania.com>
2024-05-17 09:58:06 +0200

    integrated sensor services

--- sw/atm-test-control/src/main.cpp
+++ sw/atm-test-control/src/main.cpp
@@ -16,6 +16,8 @@
 #include "light.h"
 #include "dynamics.h"
 #include "secrets.h"
+#include "setget.h"
+
 
 // Define pin-name and GPIO#. Pin # are fixed but can ofcourse be named differently
 // Motor
@@ -37,6 +39,32 @@
 #define EREAR 18
 #define TRIGHT 17 // ECHO2
 #define ERIGHT 35
+
+#define NUM_SENSORS 4
+#define TRIGGER_PIN 16
+#define ECHO_PIN 34
+#define TRIGGER_PIN2 17
+#define ECHO_PIN2 35
+#define TRIGGER_PIN3 26
+#define ECHO_PIN3 25
+#define TRIGGER_PIN4 19
+#define ECHO_PIN4 18
+
+#define COMPASS_ADDRESS 0x0d // I2C address for the compass
+// MPU-6050 I2C address is 0x68
+#define MPU6050_ADDR 0x68
+
+// Power management registers for MPU6050
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+
+volatile long distance[NUM_SENSORS];
+volatile long startTime[NUM_SENSORS];
+volatile int currentSensor = 0;
+
+// Create vecors for the ultrasouns sensor pins
+int triggerPins[NUM_SENSORS] = {TRIGGER_PIN, TRIGGER_PIN2, TRIGGER_PIN3, TRIGGER_PIN4};
+int echoPins[NUM_SENSORS] = {ECHO_PIN, ECHO_PIN2, ECHO_PIN3, ECHO_PIN4};
 
 AsyncWebServer server(80);
 
@@ -231,9 +259,177 @@ String postlog(String msg)
 //   steering.Start();
 // }
 
+void echoInterrupt()
+{
+  int i = currentSensor;
+  if (digitalRead(echoPins[i]) == HIGH)
+  {
+    // The echo pin went from LOW to HIGH: start timing
+    startTime[i] = micros();
+  }
+  else
+  {
+    // The echo pin went from HIGH to LOW: stop timing and calculate distance
+    long travelTime = micros() - startTime[i];
+    switch (i)
+    {
+    case 0:
+      globalVar_set(rawDistFront, travelTime / 29 / 2);
+      break;
+    case 1:
+      globalVar_set(rawDistRight, travelTime / 29 / 2);
+      break;
+    case 2:
+      globalVar_set(rawDistLeft, travelTime / 29 / 2);
+      break;
+    case 3:
+      globalVar_set(rawDistBack, travelTime / 29 / 2);
+      break;
+    }
+    // distance[i] = travelTime / 29 / 2;
+  }
+}
+
+void SERVICE_pollSensors(void *pvParameters)
+{
+  for (;;)
+  {
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      currentSensor = i;
+
+      // Initialize trigger and echo pins
+      pinMode(triggerPins[i], OUTPUT);
+      pinMode(echoPins[i], INPUT);
+
+      // Attach an interrupt to the echo pin
+      attachInterrupt(digitalPinToInterrupt(echoPins[i]), echoInterrupt, CHANGE);
+
+      // Send a 10 microsecond pulse to start the sensor
+      digitalWrite(triggerPins[i], HIGH);
+      delayMicroseconds(10);
+      digitalWrite(triggerPins[i], LOW);
+
+      // Wait for 100 ms before polling the next sensor
+      vTaskDelay(pdMS_TO_TICKS(100));
+    }
+  }
+}
+
+void SERVICE_readCompass(void *pvParameters)
+{
+  for (;;)
+  {
+    int x, y, z; // variables to store the compass data
+    Wire.beginTransmission(COMPASS_ADDRESS);
+    Wire.write(0x03); // Request the data starting at register 0x03
+    Wire.endTransmission();
+    Wire.requestFrom((int)COMPASS_ADDRESS, 6, true);
+    if (6 <= Wire.available())
+    {
+      x = Wire.read() << 8 | Wire.read();
+      z = Wire.read() << 8 | Wire.read();
+      y = Wire.read() << 8 | Wire.read();
+      globalVar_set(rawMagX, x);
+      globalVar_set(rawMagZ, z);
+      globalVar_set(rawMagY, y);
+      // float heading = atan2(y, x);
+      // if (heading < 0)
+      /*{
+        heading += 2 * PI;
+      }
+      Serial.print("Heading: ");
+      Serial.println(heading * 180 / PI);
+      */
+    }
+    vTaskDelay(pdMS_TO_TICKS(700));
+  }
+}
+
+void SERVICE_readAccelerometer(void *pvParameters)
+{
+  for (;;)
+  {
+    Wire.beginTransmission(MPU6050_ADDR);
+    Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom((int)MPU6050_ADDR, 14, true); // request a total of 14 registers
+
+    // read accelerometer and gyroscope data
+    int16_t AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+    globalVar_set(rawAccX, AcX);
+    int16_t AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+    globalVar_set(rawAccY, AcY);
+    int16_t AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    globalVar_set(rawAccZ, AcZ);
+    int16_t Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+    globalVar_set(rawTemp, Tmp / 34 + 365);
+    int16_t GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+    globalVar_set(rawGyX, GyX);
+    int16_t GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+    globalVar_set(rawGyY, GyY);
+    int16_t GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+    globalVar_set(rawGyZ, GyZ);
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
+void sensorSetup()
+{
+  // Initialize serial communication for debugging
+  Serial.begin(57600);
+  Wire.begin();
+  Wire.setClock(100000);
+  Wire.beginTransmission(COMPASS_ADDRESS);
+  Wire.write(0x02);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  Wire.beginTransmission(MPU6050_ADDR);
+  Wire.write(PWR_MGMT_1);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  globalVar_init();
+  // Initialize trigger and echo pins
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Attach an interrupt to the echo pin
+  attachInterrupt(digitalPinToInterrupt(ECHO_PIN), echoInterrupt, CHANGE);
+
+  // Create a task for polling the sensors
+  xTaskCreate(
+      SERVICE_pollSensors, // Task function
+      "PollSensors",       // Task name
+      2000,                // Stack size (in words, not bytes)
+      NULL,                // Task input parameter
+      2,                   // Task priority
+      NULL                 // Task handle
+  );
+
+  xTaskCreate(
+      SERVICE_readCompass, // Task function
+      "readcompass",       // Task name
+      2000,                // Stack size (in words, not bytes)
+      NULL,                // Task input parameter
+      2,                   // Task priority
+      NULL                 // Task handle
+  );
+
+  xTaskCreate(
+      SERVICE_readAccelerometer, // Task function
+      "readaccelerometer",       // Task name
+      2000,                      // Stack size (in words, not bytes)
+      NULL,                      // Task input parameter
+      2,                         // Task priority
+      NULL                       // Task handle
+  );
+}
+
+
 void setup()
 {
-
+  sensorSetup();
   Serial.begin(9600);
   cpuid = uids(); // Read the unique boxid
   Serial.print("Hello from ");
@@ -317,7 +513,7 @@ float GetCleanDist(std::vector<float> &vec){
 bool objectInRange(std::vector<float> &vec) {
   float dist = GetCleanDist(vec);
 
-  return dist > 0 && dist < rng;
+  return dist > 0 && dist < 30;
 
 }
 
@@ -356,6 +552,7 @@ void masterStrat() {
     steer.Stop();
   }
 }
+
 
 void loop()
 {
