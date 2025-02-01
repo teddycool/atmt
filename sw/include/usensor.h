d6229ae9be43d1b9d1c2a3d9ebc8f9d22ecf59da
PA <peter@atterfjall.se>
2025-02-01 19:13:21 +0100

    calssing started. realised it will be very difficult to keep interrupts separate and to avoid the sensors disturb each other. So a redesign wioll be needed. COnstructor to create a table, then you can add sensor by sensor, and then call start. One sensor at a time will then be triggered by the task and it it clear which echo is reveived.

--- sw/include/usensor.h
+++ sw/include/usensor.h
@@ -2,19 +2,24 @@
 #ifndef USENSOR_H
 #define USENSOR_H  
 
+#include "setget.h"
+
 class Usensor
 {
 public:
-  Usensor(uint8_t, uint8_t);  // declare default constructor with echo and trigger pins
+  Usensor(uint8_t, uint8_t,VarNames);  
+    // declare default constructor with echo and trigger pins, 
+    //the varname is where the values should be stored
 
 private:
-  float distance;
+  //float distance;
   const uint8_t ECHO;
   const uint8_t TRIG;
+  const VarNames name;
 
 public: 
-  void SetUp();
-  float GetDistance();
+  //void SetUp();
+  //float GetDistance();
 };
 
 #endif
