78324fdfb45f475c8fb6a1a81d2a0841da88a1ac
Teddycool <par@sundback.com>
2025-02-01 16:29:31 +0100

    Updated secrets and atmio definitions

--- sw/include/atmio.h
+++ sw/include/atmio.h
@@ -1,20 +1,28 @@
 #ifndef ATMIO_H
 #define ATMIO_H  
 
+//Hardware-settings depending on the used hardware
+
+#define USE_SERVO 0 //Use servo for steering 0 meeans no servo and a separate motor driver is used
+#define USE_LIGHT 1 
+#define USE_MPU 1
+#define USE_COMPASS 1
+
+#define USE_MQTT 1
 
 
 // Define pin-name and GPIO#. Pin # are depending on HW version
 // Naming is FIXED
 
-//Hardware version 2:
-//-------------------
+//Hardware version 2 & 3:
+//-----------------------
 
 // Motor
 #define M1E_PIN 2
 #define M1F_PIN 4
 #define M1R_PIN 5
 
-// Steering
+// Steering (without servo)
 #define SENABLE 33
 #define SRIGHT 27
 #define SLEFT 23
@@ -29,5 +37,11 @@
 #define TRIGHT 17 // ECHO2
 #define ERIGHT 35
 
+// Light (Neopixel)
+#define LIGHT_PIN 14
+#define LIGHT_NUM_LEDS 4
+
+#define SDA_PIN 21
+#define SCL_PIN 22
 
 #endif
