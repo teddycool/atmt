571785eeeceac004e49b49c8ed5a6c43b15cc9ec
PA <peter@atterfjall.se>
2025-03-01 13:08:54 +0100

    Added accsensor and all uodates required for that.  It could be that the change of setget to long instead of int coaul case some issues with other packets, needs to be verified. maybe overlaying so that both exists?

---
+++ sw/include/sensors/accsensor.h
@@ -0,0 +1,16 @@
+#ifndef ACCSENSOR_H
+#define ACCSENSOR_H
+
+//static void accel_task(void *pvParameters);
+
+class ACCsensor
+{
+public:
+    ACCsensor();
+    void Begin();
+
+private:
+ 
+};
+
+#endif
