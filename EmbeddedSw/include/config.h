804330ad7934d49fc74885d4d7b7daf108c04aeb
Pär Sundbäck <par@sundback.com>
2026-03-19 09:33:21 +0100

    Added config

---
+++ EmbeddedSw/include/config.h
@@ -0,0 +1,35 @@
+// this packet allows for individual configs of the trucks
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
+enum motorType_t
+{
+    SINGLE,
+    DIFFERENTIAL
+};
+
+enum steerType_t
+{
+    SERVO,
+    MOTOR,
+    MOTOR_PWM
+};
+
+class Config
+{
+private:
+public:
+    Config(void);
+    void Begin(void);
+
+    motorType_t get_motorType(void);
+    steerType_t get_steerType(void);
+    boolean get_servoReverse(void);
+    int get_steer_servo_min(void);
+    int get_steer_servo_max(void);
+    int get_steer_servo_adjust(void);
+};
+
+#endif
